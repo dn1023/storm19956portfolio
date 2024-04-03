a97dc2301891b21c43b2cee7d31e2fd27534c13d
bruno <brunoprotz@outlook.com>
2024-04-03 14:04:58 +0200

    Updated.

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -13,8 +13,8 @@ const CallToAction = () => {
         <SectionWrapper id='cta' className="xl:max-w-6xl my-24 lg:mx-10 xl:mx-auto mx-4 relative overflow-hidden flex flex-col-reverse md:flex-row gap-3 md:gap-0 items-center bg-gradient-to-r from-violet-700 to-purple-700 text-white rounded-2xl p-6 md:p-8 lg:px-12 lg:py-16 z-10">
             <div className="flex flex-col md:w-1/2 lg:w-3/5">
                 <h2 className="text-2xl lg:text-4xl font-extrabold">Loved this <span className="text-yellow-400">portfolio?</span> </h2>
-                <h3 className="md:text-base lg:text-xl font-medium mt-1.5">Make this <span className="text-yellow-400">yours</span> by forking.</h3>
-                <p className="text-sm md:text-base mt-2.5 md:mt-6">Fork this template on GitHub start building your own portfolio website.</p>
+                <h3 className="md:text-base lg:text-xl font-medium mt-1.5">I'd like to <span className="text-yellow-400">work</span> with you.</h3>
+                <p className="text-sm md:text-base mt-2.5 md:mt-6">Thanks for your time.</p>
                 <div className="flex items-center gap-4 my-4">
                     <Link href="https://github.com/jigar-sable/next-portfolio" target="_blank" className="py-2 px-4 bg-white text-black rounded-lg w-fit flex items-center gap-2 hover:shadow-xl transition-shadow">
                         <FaGithub />
